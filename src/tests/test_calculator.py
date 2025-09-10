066c8d264be05e77d2e17faf7160e4da98776bd5
Mamag0 <william.desgagne@hotmail.com>
2025-09-10 19:48:51 -0400

    feat: add test for calculator.py

--- src/tests/test_calculator.py
+++ src/tests/test_calculator.py
@@ -10,4 +10,26 @@ def test_app():
     my_calculator = Calculator()
     assert my_calculator.get_hello_message() == "== Calculatrice v1.0 =="
 
-# TODO: ajoutez les tests
+def test_addition():
+    my_calculator = Calculator()
+    assert my_calculator.addition(1, 2) == 3
+    assert my_calculator.addition(-1, 1) == 0
+    assert my_calculator.addition(-1, -1) == -2
+
+def test_subtraction():
+    my_calculator = Calculator()
+    assert my_calculator.subtraction(2,4) == -2
+    assert my_calculator.subtraction(2,2) == 0
+    assert my_calculator.subtraction(2,1) == 1
+
+def test_multiplication():
+    my_calculator = Calculator()
+    assert my_calculator.multiplication(2,4) == 8
+    assert my_calculator.multiplication(2,0) == 0
+    assert my_calculator.multiplication(2,-1) == -2
+
+def test_division():
+    my_calculator = Calculator()
+    assert my_calculator.division(4,2) == 2
+    assert my_calculator.division(4,0) == "Erreur : division par zéro"
+    assert my_calculator.division(4,-2) == -2
